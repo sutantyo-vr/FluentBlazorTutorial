50cbec837c3900c6ed150d0ed1aae3c0abce3ed9
Daniel Sutantyo <daniel.sutantyo@vectorrisk.com>
2024-04-17 02:24:26 +1000

    Initial commit

---
+++ FluentBlazorTutorial/Data/SampleData.cs
@@ -0,0 +1,53 @@
+﻿using FluentBlazorTutorial.Model;
+
+namespace FluentBlazorTutorial.Data;
+
+public class SampleData
+{
+	
+	public static List<Player> Players = new List<Player>
+	{
+		new Player
+		{
+			Name = "Alpha Doe",
+			Country = "Australia",
+			Placement = new List<int?> { 1, 2, 4, 12}
+		},
+		new Player
+		{
+			Name = "Beta Smith",
+			Country = "Japan",
+			Placement = new List<int?> { 12, null, 6, null }
+		},
+		new Player
+		{
+			Name = "Charlie Brown",
+			Country = "Australia",
+			Placement = new List<int?> { 9, 29, 8, null }
+		},
+		new Player
+		{
+			Name = "Delta Epsilon",
+			Country = "Greece",
+			Placement = new List<int?> { 3, null, 7, 21 }
+		},
+		new Player
+		{
+			Name = "Jane Doe",
+			Country = "Australia",
+			Placement = new List<int?> { 21, 12, null, null }
+		},
+		new Player
+		{
+			Name = "John Smith",
+			Country = "United States of America",
+			Placement = new List<int?> { 7, 16, 2, 3 }
+		},
+		new Player
+		{
+			Name = "Ron Pearl",
+			Country = "United States of America",
+			Placement = new List<int?> { 8, 1, null, 10}
+		}
+	};
+}
---
+++ FluentBlazorTutorial/Model/Player.cs
@@ -0,0 +1,9 @@
+﻿namespace FluentBlazorTutorial.Model;
+
+public class Player
+{
+	public required string Name { get; set; }
+	public required string Country { get; set; }
+	public required List<int?> Placement { get; set; }
+	
+}
